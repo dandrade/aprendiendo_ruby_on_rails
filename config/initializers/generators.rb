cd47e52c5750afff84cb2a9e4191beb7c0b6e150
Daniel Andrade <dam@unixmexico.org>
2020-08-08 10:07:47 -0500

    Initial Configuration

---
+++ config/initializers/generators.rb
@@ -0,0 +1,3 @@
+Rails.application.config.generatos do |g|
+    g.test_framework :minitest, spec: false, fixture: true
+end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,11 @@
-ENV['RAILS_ENV'] ||= 'test'
-require_relative '../config/environment'
-require 'rails/test_help'
+ENV["RAILS_ENV"] ||= "test"
+require_relative "../config/environment"
+require "rails/test_help"
+require "minitest/rails"
+require "minitest/rails/capybara"
+
+# Consider setting MT_NO_EXPECTATIONS to not add expectations to Object.
+# ENV["MT_NO_EXPECTATIONS"] = true
 
 class ActiveSupport::TestCase
   # Run tests in parallel with specified workers
